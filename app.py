1f01a28cc70c31d306c6bfcbdde06b68de904162
Henni Seppänen <104835482+henniseppis@users.noreply.github.com>
2023-03-21 13:34:41 +0200

    testing

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
+app = Flask(__name__,template_folder='templates')
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    result = db.session.execute(text("SELECT content FROM messages"))
+    messages = result.fetchall()
+    return render_template("index.html", count=len(messages), messages=messages) 
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    sql =(text("INSERT INTO messages (content) VALUES (:content)"))
+    db.session.execute(sql, {"content":content})
+    db.session.commit()
+    return redirect("/")
+
