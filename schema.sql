f6d63ac33e8e6d68f10512539717113207856d72
Henni Seppänen <104835482+henniseppis@users.noreply.github.com>
2023-03-22 12:49:58 +0200

    create tables

---
+++ schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT,
+    password TEXT,
+    role INTEGER
+    
+);
+
+CREATE TABLE skicenters (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    location TEXT
+    
+);
+
+CREATE TABLE info (
+    id SERIAL PRIMARY KEY,
+    skicenter_id INTEGER REFERENCES skicenters,
+    slopes INTEGER,
+    lifts INTEGER,
+    stars INTEGER
+    
+);
+
+
